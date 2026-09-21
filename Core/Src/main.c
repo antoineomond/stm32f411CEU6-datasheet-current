8e073eee1c743dcd574984818bcab4b5928de6ad
Antoine Omond <antoine.omond@ntymail.com>
2026-09-21 10:11:56 +0200

    Clean main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,25 +23,6 @@
 /* USER CODE BEGIN Includes */
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
-
-#define PRINT_BENCH_RES 0
-
-// Benchmark sizes
-#define BENCH_PRIME_SIZE 50000
-#define BENCH_MAT_SIZE 10500
-#define NB_ITERATIONS_MAT_MUL 2000
-
-// Benchmark correct results
-#define CORRECT_PRIME 669
-#define CORRECT_MAT_MUL 1073741824
-#define CORRECT_MAT_MUL_FLOAT_UPPER 0.525 
-#define CORRECT_MAT_MUL_FLOAT_LOWER 0.524 
-
-// Constants
-const uint32_t US = 1000000;
-const uint32_t RESET_VAL = 0xDEADBEEF; 
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -51,7 +32,12 @@ const uint32_t RESET_VAL = 0xDEADBEEF;
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define BENCH_MAT_SIZE 10500
+#define NB_ITERATIONS_MAT_MUL 2000
 
+// Benchmark correct results
+#define CORRECT_MAT_MUL_FLOAT_UPPER 0.525 
+#define CORRECT_MAT_MUL_FLOAT_LOWER 0.524 
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -68,72 +54,9 @@ const uint32_t RESET_VAL = 0xDEADBEEF;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-//#ifdef __GNUC__
-//#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-//#else
-//#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-//#endif
-//
-//PUTCHAR_PROTOTYPE
-//{
-//  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-//  return ch;
-//}
-/* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-uint32_t compute_primes(uint32_t start, uint32_t end) {
-	volatile uint32_t cpt = 0;
-	uint32_t is_prime = 1;
-	for (int k = start; k < end; k++) {
-		is_prime = 1;
-		for (int i = 2; i	< k; i++) {
-			if (k%i==0) {
-				is_prime = 0;
-			}
-		}
-		if(is_prime == 1) {
-			cpt += 1;
-		}
-	}
-	return cpt;
-}
-
-uint8_t benchmark_prime(uint32_t benchmark_size) {
-	volatile uint32_t cpt = compute_primes(2, benchmark_size);
-	uint8_t correct = 1;
-	if(cpt != CORRECT_PRIME) {
-		correct = 0;
-	}
-	return correct;
-}
-
-uint8_t benchmark_mat_mul(uint32_t benchmark_size, uint32_t nb_iteration_mat_mul) {
-	uint32_t A_value = 1UL<<15;
-	uint32_t B_value = 1UL<<15;
-	volatile uint8_t correct = 1;
-	for (int k = 0; k < nb_iteration_mat_mul; k++) {
-		// Three 32-bits matrixes of 72 elements account for 486 kB, which should account for all 8 memory banks in SRAM0 and SRAM1
-		uint32_t *A = malloc(sizeof(uint32_t)*benchmark_size);
-		uint32_t *B = malloc(sizeof(uint32_t)*benchmark_size);
-		uint32_t *C = malloc(sizeof(uint32_t)*benchmark_size);
-		for (int i = 0; i < benchmark_size; i++) {
-			A[i] = A_value;
-			B[i] = B_value;
-			//printf("B[i] %d\n", B[i]);
-		}
-		for (int i = 0; i < benchmark_size; i++) {
-			C[i] = A[i] * B[i];
-		}
-		free(A);
-		free(B);
-		free(C);
-	}
-	return correct;
-}
-
 uint8_t benchmark_mat_mul_float(uint32_t benchmark_size, uint32_t nb_iteration_mat_mul) {
 	float A_value = 1.23456789;
 	float B_value = 1.23456789;
@@ -161,76 +84,6 @@ uint8_t benchmark_mat_mul_float(uint32_t benchmark_size, uint32_t nb_iteration_m
 		free(C);
 	}
 	return correct;
-}
-
-uint8_t benchmark_mat_mul_double(uint32_t benchmark_size, uint32_t nb_iteration_mat_mul) {
-	double A_value = 1.23456789;
-	double B_value = 1.23456789;
-	volatile uint8_t correct = 1;
-	for (int k = 0; k < nb_iteration_mat_mul; k++) {
-		// Three 32-bits matrixes of 72 elements account for 486 kB, which should account for all 8 memory banks in SRAM0 and SRAM1
-		double *A = malloc(sizeof(double)*benchmark_size);
-		double *B = malloc(sizeof(double)*benchmark_size);
-		double *C = malloc(sizeof(double)*benchmark_size);
-		for (int i = 0; i < benchmark_size; i++) {
-			A[i] = A_value;
-			B[i] = B_value;
-		}
-		for (int i = 0; i < benchmark_size; i++) {
-			C[i] = A[i] * B[i] - 1;
-		}
-		// Verification
-		for (int i = 0; i < benchmark_size; i++) {
-			if(C[i] > CORRECT_MAT_MUL_FLOAT_UPPER || C[i] < CORRECT_MAT_MUL_FLOAT_LOWER) {
-				correct = 0;
-			}
-		}
-		free(A);
-		free(B);
-		free(C);
-	}
-	return correct;
-}
-
-
-void run_benchmarks() {
-	// Wait 10s after having apply conf (HAL_Delay is noop so active wait)
-	HAL_Delay(2000);
-	
-	// prime
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-	//uint32_t res_prime = benchmark_prime(BENCH_PRIME_SIZE);
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-	//HAL_Delay(100);
-	
-	// mat mul
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-	//uint32_t res_mat_mul = benchmark_mat_mul(BENCH_MAT_SIZE, NB_ITERATIONS_MAT_MUL);
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-	//HAL_Delay(100);
-	
-	//return;
-	
-	// mat mul float
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-	uint32_t res_mat_mul_float = benchmark_mat_mul_float(BENCH_MAT_SIZE, NB_ITERATIONS_MAT_MUL);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-	HAL_Delay(100);
-	
-	// mat mul double
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-	//uint32_t res_mat_mul_double = benchmark_mat_mul_double(BENCH_MAT_SIZE/2, NB_ITERATIONS_MAT_MUL);
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-	//HAL_Delay(100);
-	
-	return;
-	
-	#if PRINT_BENCH_RES
-	//printf("benchmark_prime: %d \n", res_prime);
-	//printf("benchmark_mat_mul: %d \n", res_mat_mul);
-	//printf("benchmark_mat_mul_float: %d \n", res_mat_mul_float);
-	//printf("benchmark_mat_mul_double: %d \n", res_mat_mul_double);
-	#endif
 }
 
 /* USER CODE END 0 */
@@ -268,42 +121,11 @@ int main(void)
 	__HAL_FLASH_PREFETCH_BUFFER_DISABLE();
 	__HAL_FLASH_INSTRUCTION_CACHE_DISABLE();
 	__HAL_FLASH_DATA_CACHE_DISABLE();
-	//__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-	//__HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
-	//__HAL_FLASH_DATA_CACHE_ENABLE();
-	//printf("test\n");
+	HAL_Delay(2000);
 	while(1){
-		run_benchmarks();
-		//uint32_t hclk_freq = HAL_RCC_GetHCLKFreq();
-		//printf("hclk_freq: %d\n", hclk_freq); 
-		//uint32_t pclk1_freq = HAL_RCC_GetPCLK1Freq();
-		//printf("pclk1_freq: %d\n", pclk1_freq); 
-		//uint32_t pclk2_freq = HAL_RCC_GetPCLK2Freq();
-		//printf("pclk2_freq: %d\n", pclk2_freq); 
-		//printf("\n");
-		//HAL_Delay(1000);
+		uint32_t is_correct = benchmark_mat_mul_float(BENCH_MAT_SIZE, NB_ITERATIONS_MAT_MUL);
 	}
-	
-	run_benchmarks();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
-	run_benchmarks();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-	run_benchmarks();
   /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 0);
-		//HAL_Delay(2000);
-		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 1);
-
-  }
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  /* USER CODE END 3 */
 }
 
 /**
